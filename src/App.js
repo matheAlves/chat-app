24707f96bb17e94ae5a6a1d3d7a20415bf04dddf
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-11-03 11:15:18 -0300

    feat: App component now uses react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
-import Feed from './components/Feed/ChatFeed';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RegistrationForm from './components/Feed/Login/RegistrationForm';
 
-function App() {
-  return (
-    <ChatEngine
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RegistrationForm />,
+  },
+  {
+    path: '/chat',
+    element: <ChatEngine
       height="100vh"
-      projectID="b027da1d-5f95-49a5-9f9c-fe9bf58a6630"
+      projectID="
+      b027da1d-5f95-49a5-9f9c-fe9bf58a6630"
       userName="admin"
       userSecret="12345"
-      renderChatFeed={ (chatAppProps) => <Feed { ...chatAppProps } /> }
-    />
-  );
+    />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={ router } />;
 }
 
 export default App;
