8c1a8475252aa7c0e2f9f5b209fa1deab35b2624
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-11-04 00:43:12 -0300

    feat: login screen works as registration when it's a new user

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
       path: '/chat',
       element: <ChatEngine
         height="100vh"
-        projectID="
-          b027da1d-5f95-49a5-9f9c-fe9bf58a6630"
+        projectID="b027da1d-5f95-49a5-9f9c-fe9bf58a6630"
         userName={ username }
         userSecret={ secret }
       />,
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,23 +1,88 @@
 /* eslint-disable react/jsx-max-depth */
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import MyContext from '../../Provider/Context';
 import './Form.css';
 
+const projectKey = process.env.REACT_APP_PRIVATE_KEY;
+
 function LoginForm() {
   const { username, setUsername, secret, setSecret } = useContext(MyContext);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  async function checkIfReturningUser() {
+    const isReturning = users.some((user) => user.username === username);
+    return isReturning;
+  }
 
-    navigate('/chat');
-  };
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const returningUser = await checkIfReturningUser();
+    if (!returningUser) {
+      const createUserHeaders = { headers: { 'PRIVATE-KEY': projectKey } };
+      //   const addUserToChatHeaders = { headers: {
+      //     'Project-ID': 'b027da1d-5f95-49a5-9f9c-fe9bf58a6630',
+      //     'User-Name': username,
+      //     'User-Secret': secret,
+      //   } };
+      //   const addUserToChatBody = { username };
+      const user = {
+        username,
+        secret,
+        first_name: 'null',
+        last_name: 'null',
+      };
+
+      try {
+        await axios.post('https://api.chatengine.io/users/', user, createUserHeaders);
+        // await axios.get('https://api.chatengine.io/chats/136028/people/', addUserToChatBody, addUserToChatHeaders);
+        navigate('/chat');
+      } catch (err) {
+        setError('Não foi possível criar usuario.');
+      }
+    }
+    if (returningUser) {
+      const headers = {
+        'Project-ID': 'b027da1d-5f95-49a5-9f9c-fe9bf58a6630',
+        'User-Name': username,
+        'User-Secret': secret,
+      };
+      try {
+        await axios.get('https://api.chatengine.io/users/me/', { headers });
+        navigate('/chat');
+        setError('');
+      } catch (err) {
+        setError('wrong password');
+      }
+    }
+  }
+
+  async function fetchUsers() {
+    try {
+      const config = {
+        headers: {
+          'PRIVATE-KEY': projectKey,
+        },
+      };
+      const { data } = await axios.get('https://api.chatengine.io/users/', config);
+      setUsers(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <div className="wrapper">
       <div className="form">
         <h1 className="title">Chat App</h1>
+        <h2 className="subtitle">Faça login ou crie uma conta:</h2>
         <form onSubmit={ handleSubmit }>
           <input
             type="text"
@@ -41,6 +106,7 @@ function LoginForm() {
             </button>
           </div>
         </form>
+        <h1>{error}</h1>
       </div>
     </div>
 
